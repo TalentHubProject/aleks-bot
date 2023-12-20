8a726f44628357be438b6ee4f367b6e660b4f00c
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-12-20 14:49:31 +0100

    feat(plugin/staticWelcomer): initial commit

---
+++ Aleks.Plugins/StaticWelcomer/Responders/StaticWelcomerResponder.cs
@@ -0,0 +1,38 @@
+﻿using Aleks.Business.Colors;
+using Microsoft.Extensions.Configuration;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Objects;
+using Remora.Discord.Gateway.Responders;
+using Remora.Rest.Core;
+using Remora.Results;
+
+namespace Aleks.Plugins.StaticWelcomer.Responders;
+
+/// <summary>
+///     Static welcomer responder.
+/// </summary>
+public class StaticWelcomerResponder(
+    IDiscordRestChannelAPI channelApi,
+    IConfiguration configuration)
+    : IResponder<IGuildMemberAdd>
+{
+    /// <inheritdoc />
+    public async Task<Result> RespondAsync(IGuildMemberAdd gatewayEvent, CancellationToken ct = default)
+    {
+        var channelId = configuration["WelcomerChannelId"]
+                        ?? throw new InvalidOperationException("WelcomerChannelId is not configured.");
+
+        var embed = new Embed
+        {
+            Colour = DiscordTransparentColor.Value,
+            Description = "Welcome to the server!",
+        };
+
+        var channelIdSnowflake = Snowflake.TryParse(channelId, out var snowflake)
+            ? snowflake
+            : throw new InvalidOperationException("WelcomerChannelId is not a valid snowflake.");
+
+        return (Result)await channelApi.CreateMessageAsync(channelIdSnowflake!.Value, embeds: new[] { embed }, ct: ct);
+    }
+}
